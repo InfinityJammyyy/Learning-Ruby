3051332fefd3c2fe72c3c0b0dfb7baef5488d56a
InfinityJammyyy <143579594+InfinityJammyyy@users.noreply.github.com>
2023-08-30 22:34:14 +0000

    p

--- Screwing-Around.rb
+++ Screwing-Around.rb
@@ -1,15 +1,1 @@
-loop do  
-  puts "This is a simple game of pressing the a button"
-  puts "You have 15 chances to hit that button"
 
-  choice = gets.chomp
-  
-  if choice == "a" || choice == "A"
-    puts "congratulations you pressed a"
-    break
-  else 
-    puts "invalid choice hint it's right next to s"
-    puts "how could you get that wrong"
-  end
-end
-  
