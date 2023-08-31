599e9cf67ce283fdfe6e2a4e74dfaa270dcaae53
InfinityJammyyy <143579594+InfinityJammyyy@users.noreply.github.com>
2023-08-31 02:42:10 +0000

    Update hello.rb

--- hello.rb
+++ hello.rb
@@ -6,11 +6,11 @@ loop do
   choice = gets.chomp
 
   if choice == "a"  ||  choice == "A"
-    puts "would you like to edit a pre-existing document or create a new one"
+    puts "would you like to edit a pre-existing"
+    puts "document or create a new one"
     puts "To create a new document PRESS A"
     puts "To Edit a pre-existing document PRESS C"
     puts "If you would like to exit PRESS B"
-    #continue writing here k
     
     loop do
     more_choice = gets.chomp
@@ -33,5 +33,5 @@ loop do
     break
   else 
     puts "Invalid choice. Please try again."
-  end
+    end
 end
